e07c004a381a15565113a873c66932b3985757ac
Sockzzz <86269797+Sockzzz@users.noreply.github.com>
2023-03-09 12:10:53 -0800

    Initial Commit

---
+++ pport/pportApp.swift
@@ -0,0 +1,17 @@
+//
+//  pportApp.swift
+//  pport
+//
+//  Created by Marshall Jones on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct pportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
